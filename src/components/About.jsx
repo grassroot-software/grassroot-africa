d0ab8125422d3b5507048f15ef6b5e26a7df1449
grassroot-software <126906861+grassroot-software@users.noreply.github.com>
2023-05-28 06:43:59 +0000

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,10 @@ const About = () => {
             <div className='about__section-content'>
                 <h1> About Grassroot</h1>
                 <p>
-                Prisons may seem designed to strip inhabitants of their humanity, but some individuals discover new hope and strength during their incarceration. 
+                Prisons may seem designed to strip inhabitants of their humanity, but some individuals discover new hope and strength during their incarceration.<br><br>
                 We believe  that Correctional education has the potential to lead to change in the hitherto undesirable behaviour patterns of offenders. 
-                The idea behind this scheme is to promote a paradigm shift in their thought processes, equip them with employable skills to 
-                enhance their chances of securing jobs after release from prison.
+                <br><br>The idea behind this scheme is to promote a paradigm shift in their thought processes, equip them with employable skills to 
+                enhance their chances of securing jobs after release from prison.<br><br>
                 An individuals’ understanding of reality is changed as a result of new information or situations that challenges previously held assumptions. through perspective transformation in their personal development by becoming more critically aware of their beliefs and feelings about themselves and their roles in society.
                 </p>
       
