5a7ab345d4790cb0e52ed66af846a3d5a30d33f2
grassroot-software <126906861+grassroot-software@users.noreply.github.com>
2023-05-31 05:02:05 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,25 +15,14 @@ const Footer = () => {
                 training 1 million young Africans in advanced Software Enginering courses and connect them to a 
                 network of remote employers home and abroad. To accelerate the Digital revolution in Africa and lift lives out of poverty .
                 </p>
-            </article> 
-/*
-            <article>
-                <h4> Hyperlinks</h4>
-                <a href='/' > Grassroot Africa</a>
-                <a href='https://grassroot.academy' > Grassroot Africa</a>
+           
+            
                 
                 
                 
+             
                 
-            </article>
-
-            <article>
-                <h4> Get Started</h4>
-                <a href='/signup'>Register </a>
-                <a href='https://learn.grassroot.academy/sign_in'>Login </a>
-            </article> 
-                  */
-
+              
             <article>
                 <h4> Contact Us</h4>
                 <a href='mailto:Management@grassroot.academy' target='_blank' rel='noreferrer noopener'>Send Us A Mail</a>
