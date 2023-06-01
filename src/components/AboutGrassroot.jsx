f57018a9aeb40200e8aed2ccc9006eedf6e443ff
Bright Kportiklah <littyofficials@gmail.com>
2023-06-01 15:08:37 +0000

    update page

--- src/components/AboutGrassroot.jsx
+++ src/components/AboutGrassroot.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Link } from "react-router-dom";
 import MakeDonation from "./makeDonation";
 
 
@@ -81,9 +80,6 @@ const AboutGrassroot = () => {
                 </ul>
                <br />
                <br />
-
-
-                <MakeDonation />
 
 
                 <br />
